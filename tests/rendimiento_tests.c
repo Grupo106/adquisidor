1249822e7ea798f99ff9c8f9a6e1a450a0e6da20
Yonatan Romero <yromero@openmailbox.org>
2016-06-21 14:06:09 -0300

    Pruebas rendimiento

---
+++ tests/rendimiento_tests.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+
+#include "db.h"
+#include "adquisidor.h"
+
+void main() {
+    int cantidad = 1800;
+    struct paquete paquete;
+    paquete.src.s_addr = htonl(83886339);
+    paquete.dst.s_addr = htonl(83888127);
+    paquete.sport = 138;
+    paquete.dport = 138;
+    paquete.protocol = 17;
+    paquete.bytes = 261;
+    bd_conectar();
+    while(cantidad--) bd_insertar(&paquete); 
+    bd_commit();
+    bd_desconectar();
+}
+
